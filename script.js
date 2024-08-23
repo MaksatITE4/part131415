a499d0d59964f327b65d05c2c4b888f0ec78829f
MaqsatITE4 <maksatmuslim04@gmail.com>
2024-08-23 22:42:40 +0500

    Java Script lessons

---
+++ script.js
@@ -0,0 +1,68 @@
+// 15 Easy Tasks
+
+let num = 42 
+console.log(num)
+
+let str = "Hello world"
+console.log(str)
+
+let isTrue = true 
+console.log(isTrue)
+
+let a = 5 
+let b = 3 
+console.log(a + b)
+
+const pi = 3.14 
+console.log(pi)
+
+let name = "Maqsat"
+console.log(name)
+
+let age = 22
+console.log(age)
+
+let message = "Welcome to my Website"
+console.log(message)
+
+let x = 10
+let y = 5 
+console.log(x -y)
+
+let firstName = "Maqsat"
+let lastName = "Amangeldi"
+console.log(firstName + " " + lastName)
+
+let width = 8
+let height = 16
+console.log(width * height) 
+
+let greeting = "hello"
+let names = "Maqsat" 
+console.log(greeting, names)
+
+let radius = 5 
+console.log(3.14 * 25)
+
+let num1 = 10 
+let num2 = 3 
+console.log(num1 / num2)
+
+let num3 = 7 
+let num4 = 2 
+console.log(num3%num4)
+
+//2 Medium Tasks
+let celsia = 18 
+console.log("Температура в градусах Фаренгейта: ", (celsia * 9/5) + 32)
+console.log("Температура в градусах Кельвина: " , (celsia + 273.15))
+
+let deposit = 5000
+let rate = 14
+console.log("Сумма вклада через год: ", (deposit * rate * 30) / 365)
+console.log("Сумма вклада через два года: ", (deposit * rate * 60) / 365)
+console.log("Сумма вклада через три года: ", (deposit * rate * 90) / 365)
+
+
+
+
